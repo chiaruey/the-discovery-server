3a2113d078669a2a0237b6fe503666befa523a14
rueytpe <rueytpe@gmail.com>
2020-06-06 12:54:42 -0700

    first commit

---
+++ src/main/java/com/ruey/discovery/TheDiscoveryServerApplication.java
@@ -0,0 +1,13 @@
+package com.ruey.discovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheDiscoveryServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheDiscoveryServerApplication.class, args);
+	}
+
+}
